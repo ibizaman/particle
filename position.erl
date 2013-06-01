88c4c1385866bc0c7bb57d78badac3e7b4fd7e48
Pierre Penninckx <ibizapeanut@gmail.com>
2013-06-01 23:35:08 +0200

    adds position module holding x,y location

---
+++ position.erl
@@ -0,0 +1,17 @@
+-module(position).
+
+-export([new/2, x/1, y/1, move/2, distance/2]).
+
+-record(position, {x = 0, y = 0}).
+
+new(X, Y) when is_number(X), is_number(Y) ->
+    #position{x = X, y = Y}.
+
+x(#position{x = X}) ->
+    X.
+
+y(#position{y = Y}) ->
+    Y.
+
+distance(#position{x = Ax, y = Ay}, #position{x = Bx, y = By}) ->
+    math:sqrt(math:pow(Ax-Bx, 2) + math:pow(Ay-By, 2)).
