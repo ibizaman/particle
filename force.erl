3c3299cebcc36b4045ee612dd28149a420740248
Pierre Penninckx <ibizapeanut@gmail.com>
2013-06-02 00:42:32 +0200

    refactors compute/3 to return force for both position

--- force.erl
+++ force.erl
@@ -24,11 +24,12 @@ add(ForceA, ForceB) ->
     Vector = vector:add(direction(ForceA), direction(ForceB)),
     new(vector:unitLength(Vector), vector:len(Vector)).
 
-compute(ForceType, {from, LocationA, PropertiesA}, {appliedTo, LocationB, PropertiesB}) ->
-    Vector = vector:fromTo(LocationA, LocationB),
-    Distance = position:distance(LocationA, LocationB),
+compute(ForceType, {PositionA, PropertiesA}, {PositionB, PropertiesB}) ->
+    Vector = vector:fromTo(PositionA, PositionB),
+    Distance = position:distance(PositionA, PositionB),
     Intensity = force:ForceType(Distance, PropertiesA, PropertiesB),
-    new(Vector, Intensity).
+    Force = new(Vector, Intensity),
+    {invertDirection(Force), Force}.
 
 electrostatic(Distance, ChargeA, ChargeB) ->
     ChargeA * ChargeB * ?Kc / math:pow(Distance, 3).
